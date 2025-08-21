e57b4ecca874e5326756590c3064b3a615bc10ad
Juliacarol09 <js0424711@gmail.com>
2025-08-21 15:18:21 -0300

    Primeiro commit

---
+++ atividade 4/script.js
@@ -0,0 +1,8 @@
+function mostrarSaudaçao() {
+
+    let = "Julia";
+
+    const paragrafo= document.getElementById("mensagem");
+
+    paragrafo.textContent = "olá," Julia "! seja bem-vindo(a!";
+}
---
+++ atividade 5/script.js
@@ -0,0 +1,7 @@
+function apresentar() {
+    let nome = "julia";
+    let hobby = "treinar musculação";
+
+  const paragrafo = document.getElementById("mensagem");
+  paragrafo.textContent = "ola, eu sou" + julia + ' e gosto muito de " treinar musculação "!';
+}
